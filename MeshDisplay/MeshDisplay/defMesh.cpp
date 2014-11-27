23c4b6081ff08818857054ee7271243707087af3
Alex Huot <huot.alex@gmail.com>
2014-11-27 12:15:31 -0500

    Bounding boxes for picking, implemented

--- MeshDisplay/MeshDisplay/defMesh.cpp
+++ MeshDisplay/MeshDisplay/defMesh.cpp
@@ -5,6 +5,7 @@ DefMesh::DefMesh(std::string filename)
 {
     pmodel = new PLYmodel;
     pmodel->Load((char*)filename.c_str());
+    makeBBox();
     
     //pmodel = NULL;
     //if (!pmodel) {	/* load up the model */
@@ -48,7 +49,11 @@ DefMesh::DefMesh(std::string filename)
 
 void DefMesh::glDraw(int type)
 {
+    glColor3f(0.5, 0.5, 0.5);
     pmodel->Draw();
+    glColor3f(0.5, 0.0, 0.0);
+    bbox.draw();
+    
     //switch(type){
     //case 0:
     //    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); break;
@@ -68,4 +73,37 @@ void DefMesh::glDraw(int type)
     //glPopMatrix();
     //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+}
+
+void DefMesh::makeBBox()
+{
+    // Find meshes bounding box
+    bbox.xMin = bbox.yMin = bbox.zMin =  FLT_MAX;
+    bbox.xMax = bbox.yMax = bbox.zMax = -FLT_MAX;
+
+    std::cout << "xMin: " << bbox.xMin << " yMin: " << bbox.yMin << " zMin: " << bbox.zMin << std::endl
+              << "xMax: " << bbox.xMax << " yMax: " << bbox.yMax << " zMax: " << bbox.zMax << std::endl
+              << "xCen: " << bbox.xCen << " yCen: " << bbox.yCen << " zCen: " << bbox.zCen << std::endl;
+        
+    for (unsigned i = 0; i < pmodel->TotalConnectedPoints; i++){
+        float v_x = pmodel->Vertex_Buffer[0 + (i * 3)];
+        float v_y = pmodel->Vertex_Buffer[1 + (i * 3)];
+        float v_z = pmodel->Vertex_Buffer[2 + (i * 3)];
+
+        if (v_x < bbox.xMin) bbox.xMin = v_x;
+        if (v_y < bbox.yMin) bbox.yMin = v_y;
+        if (v_z < bbox.zMin) bbox.zMin = v_z;
+
+        if (v_x > bbox.xMax) bbox.xMax = v_x;
+        if (v_y > bbox.yMax) bbox.yMax = v_y;
+        if (v_z > bbox.zMax) bbox.zMax = v_z;
+    }
+
+    bbox.xCen = (bbox.xMin + bbox.xMax) / 2.0;
+    bbox.yCen = (bbox.yMin + bbox.yMax) / 2.0;
+    bbox.zCen = (bbox.zMin + bbox.zMax) / 2.0;
+
+    std::cout << "xMin: " << bbox.xMin << " yMin: " << bbox.yMin << " zMin: " << bbox.zMin << std::endl
+              << "xMax: " << bbox.xMax << " yMax: " << bbox.yMax << " zMax: " << bbox.zMax << std::endl
+              << "xCen: " << bbox.xCen << " yCen: " << bbox.yCen << " zCen: " << bbox.zCen << std::endl;
 }
--- MeshDisplay/MeshDisplay/defMesh.h
+++ MeshDisplay/MeshDisplay/defMesh.h
@@ -2,21 +2,75 @@
 #ifndef MESH_H
 #define MESH_H
 #include "plyModel.h"
+#include "GL/glut.h"
 #include <string>
+#include <limits>
+#include <iostream>
 
 // TODO:
 // - Add center of mass
 // - Add boolean isPicked
 // - Transform all points on mouse-movement
 
+struct BBox{
+    float xMin, yMin, zMin, 
+          xMax, yMax, zMax,
+          xCen, yCen, zCen;
+    
+    void draw(){
+        glTranslated(xMin, yMin, zMin);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMin, -yMin, -zMin);
+        
+        glTranslated(xMax, yMin, zMin);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMax, -yMin, -zMin);
+
+        glTranslated(xMax, yMin, zMax);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMax, -yMin, -zMax);
+
+        glTranslated(xMin, yMin, zMax);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMin, -yMin, -zMax);
+        
+        glTranslated(xMin, yMax, zMin);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMin, -yMax, -zMin);
+
+        glTranslated(xMax, yMax, zMin);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMax, -yMax, -zMin);
+
+        glTranslated(xMax, yMax, zMax);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMax, -yMax, -zMax);
+
+        glTranslated(xMin, yMax, zMax);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xMin, -yMax, -zMax);
+
+        glTranslated(xCen, yCen, zCen);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-xCen, -yCen, -zCen);
+
+
+    }
+};
+
 class DefMesh
 {
 public:
     PLYmodel *pmodel;
     GLuint    mode;
+    BBox      bbox;
+    bool      isSelected;
 
     DefMesh(std::string filename);
     ~DefMesh() { delete pmodel; }
+
     void glDraw(int type);
+
+    void makeBBox();
 };
 #endif
